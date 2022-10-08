ec4b4cdb8eeb76f64839019b075090386a603600
Dino Rossegger <dino.rossegger@tutanota.com>
2022-10-08 12:10:28 -0700

    nvim initial setup

---
+++ init.lua
@@ -0,0 +1,116 @@
+-- Runtime path setup
+package.path=package.path .. ';/home/dino/.config/nvim/?.lua'
+--
+
+vim.o.syntax='on'
+vim.o.errorbells=false
+vim.o.smartcase=true
+vim.o.mouse='a'
+vim.o.undodir = vim.fn.stdpath('config') .. '/undodir'
+vim.o.undofile = true
+vim.o.incsearch = true
+vim.o.hidden = true
+vim.o.completeopt='menuone,noinsert,noselect'
+vim.bo.autoindent = true
+vim.bo.smartindent = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.wo.number = true
+vim.wo.relativenumber = false 
+vim.wo.signcolumn = 'yes'
+vim.wo.wrap = false
+vim.o.swapfile=false
+vim.g.mapleader=' '
+vim.g.maplocalleader="'"
+
+local key_mapper = function(mode, key, result)
+  vim.api.nvim_set_keymap(
+    mode,
+    key,
+    result,
+    {noremap = true, silent = true}
+  )
+end
+
+key_mapper('', '<up>', '<nop>')
+key_mapper('', '<down>', '<nop>')
+key_mapper('', '<left>', '<nop>')
+key_mapper('', '<right>', '<nop>')
+
+-- Plugin Manager Packer
+--
+--
+--
+local vim = vim
+local execute = vim.api.nvim_command
+local fn = vim.fn
+-- ensure that packer is installed
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+    execute 'packadd packer.nvim'
+end
+vim.cmd('packadd packer.nvim')
+local packer = require'packer'
+local util = require'packer.util'
+packer.init({
+  package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
+})
+--- startup and add configure plugins
+packer.startup(function()
+  local use = use
+  -- add you plugins here like:
+  -- use 'neovim/nvim-lspconfig'
+  use 'lervag/vimtex'
+  use 'tpope/vim-surround'
+  use 'vim-pandoc/vim-pandoc-syntax'
+  use 'vim-pandoc/vim-pandoc'
+  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+  use 'sheerun/vim-polyglot'
+
+  -- theme
+  use 'tjdevries/colorbuddy.nvim'
+  use 'bkegley/gloombuddy'
+
+  -- sneaking some formatting in here too
+  use {'prettier/vim-prettier', run = 'yarn install' }
+
+  use 'neovim/nvim-lspconfig'
+  use 'nvim-lua/completion-nvim'
+  use 'anott03/nvim-lspinstall'
+
+  use 'preservim/nerdtree'
+  use 'ryanoasis/vim-devicons'
+  use {'neoclide/coc.nvim', branch='release'}
+end 
+)
+
+vim.g.colors_name = 'gloombuddy'
+
+-- Plugin config
+vim.g.vim_markdown_math = 1
+vim.g.tex_flavor='latex'
+vim.g.vimtex_view_method='zathura'
+vim.g.vimtex_compiler_engine='lualatex'
+vim.g.vimtex_compiler_progname='nvr'
+-- LSP
+local lspconfig = require'lspconfig'
+local completion = require'completion'
+local function custom_on_attach(client)
+  print('Attaching to ' .. client.name)
+  completion.on_attach(client)
+end
+local default_config = {
+  on_attach = custom_on_attach,
+}
+-- setup language servers here
+lspconfig.tsserver.setup(default_config)
+
+-- key_mapper('n','<Leader>n',':NERDTree<CR>')
+key_mapper('n','<Leader>t',':NERDTreeToggle<CR>')
+key_mapper('n','<Leader>f',':NERDTreeFind<CR>')
+
+-- Include COC config
+require 'include.coc' 
